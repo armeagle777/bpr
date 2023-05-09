6a925ae8a21d43156cdac6250738796b54a6e73e
armeaGle777 <tyeranyan@gmail.com>
2023-05-09 20:15:26 +0400

    fix: real server services

---
+++ server/src/modules/companies/services.js
@@ -0,0 +1,40 @@
+const axios = require("axios");
+
+const ApiError = require("../../exceptions/api-error");
+
+const getCompaniesBySsnDb = async (ssn) => {
+  const taxUrl = process.env.PETREGISTR_URL;
+
+  const body = JSON.stringify({
+    jsonrpc: "2.0",
+    id: 1,
+    method: "person_info",
+    params: { ssn },
+  });
+  var config = {
+    method: "post",
+    url: taxUrl,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: body,
+  };
+  const { data } = await axios(config);
+  console.log(data);
+  if (data.length === 0) {
+    return [];
+  }
+
+  const {
+    result: { person },
+  } = data;
+
+  const { companies, ...rest } = { ...person };
+  const result = { ...rest, companies: Object.values(companies) };
+
+  return result;
+};
+
+module.exports = {
+  getCompaniesBySsnDb,
+};
