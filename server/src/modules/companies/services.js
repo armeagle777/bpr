8eb999d3f146b7f3501c8b0b68a6dd19401778c0
armeagle777 <t.yeranyan@gmail.com>
2023-06-23 21:20:05 +0400

    fix: service file create function from service.local

--- server/src/modules/companies/services.js
+++ server/src/modules/companies/services.js
@@ -1,40 +1,39 @@
-const axios = require("axios");
+const axios = require('axios');
 
-const ApiError = require("../../exceptions/api-error");
+const ApiError = require('../../exceptions/api-error');
 
 const getCompaniesBySsnDb = async (ssn) => {
-  const taxUrl = process.env.PETREGISTR_URL;
+    const taxUrl = process.env.PETREGISTR_URL;
 
-  const body = JSON.stringify({
-    jsonrpc: "2.0",
-    id: 1,
-    method: "person_info",
-    params: { ssn },
-  });
-  var config = {
-    method: "post",
-    url: taxUrl,
-    headers: {
-      "Content-Type": "application/json",
-    },
-    data: body,
-  };
-  const { data } = await axios(config);
+    const body = JSON.stringify({
+        jsonrpc: '2.0',
+        id: 1,
+        method: 'person_info',
+        params: { ssn },
+    });
+    var config = {
+        method: 'post',
+        url: taxUrl,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        data: body,
+    };
+    const { data } = await axios(config);
 
-  if (data.error) {
-    return [];
-  }
-  console.log("data", data);
-  const {
-    result: { person },
-  } = data;
+    if (data.error) {
+        return [];
+    }
+    const {
+        result: { person },
+    } = data;
 
-  const { companies, ...rest } = { ...person };
-  const result = { ...rest, companies: Object.values(companies) };
+    const { companies, ...rest } = { ...person };
+    const result = { ...rest, companies: Object.values(companies) };
 
-  return result;
+    return result;
 };
 
 module.exports = {
-  getCompaniesBySsnDb,
+    getCompaniesBySsnDb,
 };
