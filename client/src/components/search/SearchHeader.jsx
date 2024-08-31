aa06ff4eba10deac6b91bc209422904e7dc09bd5
Tigran Yeranyan <tigran.e@purlin.com>
2024-09-01 01:14:24 +0400

    fix: row open

--- client/src/components/search/SearchHeader.jsx
+++ client/src/components/search/SearchHeader.jsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { PersonSearch, RestartAlt } from "@mui/icons-material";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
-import TextField from "@mui/material/TextField";
+import { Box, Button, Stack, TextField, Slide } from "@mui/material";
+import { FocusTrap } from "@mui/base/FocusTrap";
 
 const initialFilterProps = {
   ssn: "",
@@ -16,6 +14,43 @@ const initialFilterProps = {
 
 const SearchHeader = ({ setSearchParams, changePage }) => {
   const [filterProps, setFilterProps] = useState(initialFilterProps);
+  const [isNameRowOpen, setIsNameRowOpen] = useState(
+    !!filterProps.firstName.length
+  );
+  const [foxusOpen, setFoxusOpen] = useState(false);
+  const onNameFocus = () => {
+    setIsNameRowOpen(true);
+    setTimeout(() => {
+      setFoxusOpen(true);
+    }, 300);
+  };
+  useEffect(() => {
+    let timeoutId;
+    if (filterProps.firstName.length === 0) {
+      setIsNameRowOpen(false);
+      timeoutId = setTimeout(() => {
+        setFoxusOpen(false);
+      }, 700);
+    } else if (filterProps.firstName.length > 0 && !isNameRowOpen) {
+      onNameFocus();
+    }
+
+    return () => clearTimeout(timeoutId);
+  }, [filterProps.firstName]);
+
+  const isResetBtnDisabled = useMemo(
+    () => !Object.values(filterProps).filter((value) => !!value).length,
+    [filterProps]
+  );
+  const isSearchBtnActive = useMemo(() => {
+    const { ssn, lastName, firstName, documentNumber } = filterProps;
+    return (
+      (ssn && ssn.length === 10) ||
+      !!documentNumber ||
+      (!!firstName && !!lastName)
+    );
+  }, [filterProps]);
+
   const onInputChange = (event) => {
     const { name, value } = event.target;
     setFilterProps({ ...filterProps, [name]: value.trim().toUpperCase() });
@@ -58,56 +93,76 @@ const SearchHeader = ({ setSearchParams, changePage }) => {
           name="firstName"
           onChange={onInputChange}
           value={filterProps.firstName}
+          onFocus={onNameFocus}
         />
-        <TextField
-          type="search"
-          id="lastName"
-          name="lastName"
-          label="Ազգանուն"
-          onChange={onInputChange}
-          value={filterProps.lastName}
-        />
-        <TextField
-          type="search"
-          label="Հայրանուն"
-          id="patronomicName"
-          name="patronomicName"
-          onChange={onInputChange}
-          value={filterProps.patronomicName}
-        />
-        <TextField
-          type="date"
-          id="birthDate"
-          name="birthDate"
-          label="Ծննդ․ թիվ"
-          InputLabelProps={{
-            shrink: true,
+
+        <Box
+          sx={{
+            width: isNameRowOpen ? "540px" : 0,
+            display: "flex",
+            flexDirection: "row",
+            overflow: "hidden",
+            transition: "width 1.3s ease-in-out",
           }}
-          onChange={onInputChange}
-          value={filterProps.birthDate}
-        />
-        <TextField
-          type="search"
-          label="Անձնագիր"
-          id="documentNumber"
-          name="documentNumber"
-          onChange={onInputChange}
-          value={filterProps.documentNumber}
-        />
-        <TextField
-          id="ssn"
-          name="ssn"
-          label="ՀԾՀ"
-          type="search"
-          value={filterProps.ssn}
-          onChange={onInputChange}
-        />
+        >
+          <TextField
+            type="search"
+            id="lastName"
+            name="lastName"
+            label="Ազգանուն"
+            onChange={onInputChange}
+            value={filterProps.lastName}
+          />
+          <TextField
+            type="search"
+            label="Հայրանուն"
+            id="patronomicName"
+            name="patronomicName"
+            onChange={onInputChange}
+            value={filterProps.patronomicName}
+          />
+          <TextField
+            type="date"
+            id="birthDate"
+            name="birthDate"
+            label="Ծննդ․ թիվ"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            onChange={onInputChange}
+            value={filterProps.birthDate}
+          />
+        </Box>
+
+        {!foxusOpen && (
+          <FocusTrap disableEnforceFocus open>
+            <>
+              <TextField
+                type="search"
+                label="Անձնագիր"
+                id="documentNumber"
+                name="documentNumber"
+                onChange={onInputChange}
+                value={filterProps.documentNumber}
+              />
+              <TextField
+                id="ssn"
+                name="ssn"
+                label="ՀԾՀ"
+                type="search"
+                value={filterProps.ssn}
+                onChange={onInputChange}
+              />
+            </>
+          </FocusTrap>
+        )}
         <Button
           size="large"
           sx={{ py: 2 }}
           color="primary"
           variant="contained"
           onClick={handleSubmit}
+          disabled={!isSearchBtnActive}
         >
           <PersonSearch />
         </Button>
@@ -117,6 +172,7 @@ const SearchHeader = ({ setSearchParams, changePage }) => {
           color="error"
           title="Մաքրել"
           variant="contained"
+          disabled={isResetBtnDisabled}
           onClick={handleClearButton}
         >
           <RestartAlt />
