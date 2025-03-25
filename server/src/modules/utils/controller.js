2b7a52c9050dcaebe5b0e12d2316ce9f4a0fa817
armeagle777 <tyeranyan@yahoo.com>
2025-03-25 22:14:18 +0400

    ip

--- server/src/modules/utils/controller.js
+++ server/src/modules/utils/controller.js
@@ -1,6 +1,8 @@
 const getRequestIp = (req,res,next)=>{
     try {
-        const ip = req.headers['x-forwarded-for']?.split(',')[0] || req.socket.remoteAddress;
+        const ip = req.headers['x-forwarded-for']?.split(',')[0] ||
+            req.connection.remoteAddress ||
+            req.socket.remoteAddress;
         res.status(200).json({ip});
     } catch (err) {
         console.log("Error crating User:", err);
