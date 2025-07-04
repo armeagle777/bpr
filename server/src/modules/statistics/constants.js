cf781806f248ed992f1c2641765092fa0a1140ce
armeagle777 <tyeranyan@yahoo.com>
2025-07-04 17:33:07 +0400

    fix: statistics query

--- server/src/modules/statistics/constants.js
+++ server/src/modules/statistics/constants.js
@@ -26,27 +26,35 @@ GROUP BY YEAR(asylum_general_stats.period)`;
 
 const statisticsBaseQuery = `SELECT
 EAEU_STAT.country_arm,
-count(EAEU_STAT.personal_id) AS TOTAL_APPLICATIONS,
-count(if(EAEU_STAT.sex = 1, EAEU_STAT.personal_id, null)) AS TOTAL_MALE,
-count(if(EAEU_STAT.sex = 2, EAEU_STAT.personal_id, null)) AS TOTAL_FEMALE,
-count(if(EAEU_STAT.sex = 1  AND EAEU_STAT.age <= 13, EAEU_STAT.personal_id, null)) AS MALE_0_13,
+
 count(if(EAEU_STAT.sex = 2  AND EAEU_STAT.age <= 13, EAEU_STAT.personal_id, null)) AS FEMALE_0_13,
+count(if(EAEU_STAT.sex = 1  AND EAEU_STAT.age <= 13, EAEU_STAT.personal_id, null)) AS MALE_0_13,
 count(if( EAEU_STAT.age <= 13, EAEU_STAT.personal_id, null)) AS TOTAL_0_13,
-count(if(EAEU_STAT.sex = 1 AND EAEU_STAT.age > 13 AND EAEU_STAT.age <= 17, EAEU_STAT.personal_id, null)) AS MALE_14_17,
+
 count(if(EAEU_STAT.sex = 2 AND EAEU_STAT.age > 13 AND EAEU_STAT.age <= 17, EAEU_STAT.personal_id, null)) AS FEMALE_14_17,
+count(if(EAEU_STAT.sex = 1 AND EAEU_STAT.age > 13 AND EAEU_STAT.age <= 17, EAEU_STAT.personal_id, null)) AS MALE_14_17,
 count(if(EAEU_STAT.age > 13 AND EAEU_STAT.age <= 17, EAEU_STAT.personal_id, null)) AS TOTAL_14_17,
-count(if(EAEU_STAT.sex = 1 AND EAEU_STAT.age > 17 AND EAEU_STAT.age <= 34, EAEU_STAT.personal_id, null)) AS MALE_18_34,
+
 count(if(EAEU_STAT.sex = 2 AND EAEU_STAT.age > 17 AND EAEU_STAT.age <= 34, EAEU_STAT.personal_id, null)) AS FEMALE_18_34,
+count(if(EAEU_STAT.sex = 1 AND EAEU_STAT.age > 17 AND EAEU_STAT.age <= 34, EAEU_STAT.personal_id, null)) AS MALE_18_34,
 count(if(EAEU_STAT.age > 17 AND EAEU_STAT.age <= 34, EAEU_STAT.personal_id, null)) AS TOTAL_18_34,
-count(if(EAEU_STAT.sex = 1 AND EAEU_STAT.age > 34 AND EAEU_STAT.age <= 64, EAEU_STAT.personal_id, null)) AS MALE_35_64,
+
 count(if(EAEU_STAT.sex = 2 AND EAEU_STAT.age > 34 AND EAEU_STAT.age <= 64, EAEU_STAT.personal_id, null)) AS FEMALE_35_64,
-count(if(EAEU_STAT.age > 34 AND EAEU_STAT.age <= 64, EAEU_STAT.personal_id, null)) AS TOTAL_35_64,    
-count(if(EAEU_STAT.sex = 1 AND EAEU_STAT.age >= 2020, EAEU_STAT.personal_id, null)) AS MALE_UNKNOWN,
+count(if(EAEU_STAT.sex = 1 AND EAEU_STAT.age > 34 AND EAEU_STAT.age <= 64, EAEU_STAT.personal_id, null)) AS MALE_35_64,
+count(if(EAEU_STAT.age > 34 AND EAEU_STAT.age <= 64, EAEU_STAT.personal_id, null)) AS TOTAL_35_64,
+
 count(if(EAEU_STAT.sex = 2 AND EAEU_STAT.age >= 2020, EAEU_STAT.personal_id, null)) AS FEMALE_UNKNOWN,
+count(if(EAEU_STAT.sex = 1 AND EAEU_STAT.age >= 2020, EAEU_STAT.personal_id, null)) AS MALE_UNKNOWN,
 count(if(EAEU_STAT.age >= 2020, EAEU_STAT.personal_id, null)) AS TOTAL_UNKNOWN,
-count(if(EAEU_STAT.sex = 1 AND EAEU_STAT.age > 64, EAEU_STAT.personal_id, null)) AS MALE_65_PLUS,
+
 count(if(EAEU_STAT.sex = 2 AND EAEU_STAT.age > 64, EAEU_STAT.personal_id, null)) AS FEMALE_65_PLUS,
-count(if(EAEU_STAT.age > 64, EAEU_STAT.personal_id, null)) AS TOTAL_65_PLUS`;
+count(if(EAEU_STAT.sex = 1 AND EAEU_STAT.age > 64, EAEU_STAT.personal_id, null)) AS MALE_65_PLUS,
+count(if(EAEU_STAT.age > 64, EAEU_STAT.personal_id, null)) AS TOTAL_65_PLUS,
+
+count(if(EAEU_STAT.sex = 2, EAEU_STAT.personal_id, null)) AS TOTAL_FEMALE,
+count(if(EAEU_STAT.sex = 1, EAEU_STAT.personal_id, null)) AS TOTAL_MALE,
+count(EAEU_STAT.personal_id) AS TOTAL_APPLICATIONS
+`;
 
 const decTypeTableNameMap = {
   1: "refugees",
